930af708403ac64910efa7566e5c6f522999e7a5
KdauntlN <henryknzed@gmail.com>
2025-03-22 22:31:22 +1100

    Overhauling everything

--- src/main.c
+++ src/main.c
@@ -1,42 +1,80 @@
-#include <stdio.h>
+#include <math.h>
 #include "raylib.h"
-#include "character.h"
-#include "constants.h"
+
+// Declare screen size constants
+const int SCREEN_WIDTH = 270;
+const int SCREEN_HEIGHT = 480;
+
+// Create a struct to keep track of character values
+typedef struct {
+
+    // Declare variables related to positioning and movement
+    float x, y;
+    float vx, vy;
+    int width, height;
+    float speed;
+    float gravity;
+    float rotation;
+    
+    // Declare textures
+    Texture2D jetpackOff;
+    Texture2D jetpackOn1;
+    Texture2D jetpackOn2;
+
+} Character;
+
+Character initCharacter() {
+
+    // Zero-initialise the character struct
+    Character character = { 0 };
+
+    // Define width and height
+    character.width = 20;
+    character.height = 20;
+
+    // Place the character in the centre of the screen
+    character.x = SCREEN_WIDTH / 2 - character.width / 2;
+    character.y = SCREEN_HEIGHT / 2 - character.height / 2;
+
+    // Define speed in x and y directions
+    character.vx = 0;
+    character.vy = 0;
+
+    // Initialize remaining variables
+    character.speed = 0.2;
+    character.gravity = 0.1;
+    character.rotation = 0;
+
+    // Initialise textures
+    character.jetpackOff = LoadTexture("assets/jetpack-off.png");
+    character.jetpackOn1 = LoadTexture("assets/jetpack-on-1.png");
+    character.jetpackOn2 = LoadTexture("assets/jetpack-on-2.png");
+
+    // Return the charater
+    return character;
+}
 
 int main() {
-    SetTargetFPS(60);
+
+    // Open the window
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Skybound");
 
-    Texture2D jetpackOn1 = LoadTexture("assets/jetpack-on-1.png");
-    Texture2D jetpackOn2 = LoadTexture("assets/jetpack-on-2.png");
-    Texture2D jetpackOff = LoadTexture("assets/jetpack-off.png");
+    // Create the player character
+    Character player = initCharacter();
 
-    float frameTime = 0.0f;
-    int currentFrame = 0;
-
+    // Keep the window open
     while (!WindowShouldClose()) {
-        frameTime += GetFrameTime();
-
-        if (frameTime > 0.2f) {
-            currentFrame = !currentFrame;
-            frameTime = 0.0f;
-        }
-
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        bool isJetpackOn = IsKeyDown(KEY_W);
-        if (isJetpackOn) {
-            drawCharacter(currentFrame ? jetpackOn2:jetpackOn1, 20, 20, WHITE);
-        }
-        else {
-            drawCharacter(jetpackOff, 20, 20, WHITE);
-        }
+        ClearBackground(RAYWHITE);  // Clear the screen
         EndDrawing();
     }
 
-    UnloadTexture(jetpackOn1);
-    UnloadTexture(jetpackOn2);
-    UnloadTexture(jetpackOff);
+    // Unload textures
+    UnloadTexture(player.jetpackOff);
+    UnloadTexture(player.jetpackOn1);
+    UnloadTexture(player.jetpackOn2);
+
     CloseWindow();
+
     return 0;
 }
